0c3af154daa0b5bbf61f528e49bb1e8bf2e3ae89
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2023-07-02 14:01:32 +0900

    COMMIT

--- 01_variable/02_Variable.py
+++
@@ -1,9 +0,0 @@
-import sys
-print(sys.getrefcount(2023))
-x=2023
-print(sys.getrefcount(2023))
-y=2023
-print(sys.getrefcount(2023))
-print(sys.getrefcount(1))
-# is 연산자 : 객체가 같은 타입인지 확인하는 연산자
-print(x is y)
